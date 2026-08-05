b79cd55c8976c02d11db14b2d4831118c6d7c971
ibtikarysa <ibtikarysa@gmail.com>
2026-08-05 23:14:41 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,18 @@
 document.addEventListener("DOMContentLoaded", function () {
+
     lucide.createIcons();
+
+    const menuToggle = document.getElementById("menuToggle");
+    const navMenu = document.getElementById("navMenu");
+
+    menuToggle.addEventListener("click", function () {
+
+        if (navMenu.style.display === "flex") {
+            navMenu.style.display = "none";
+        } else {
+            navMenu.style.display = "flex";
+        }
+
+    });
+
 });
